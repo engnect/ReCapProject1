13de7873890638e144082d365698318582ccf296
Engin Celal Taşkın <engin48ect@gmail.com>
2021-04-08 01:52:53 +0300

    15.Gün ödev

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -13,6 +13,10 @@ using Business.ValidationRules.FluentValidation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Aspects.Autofac.Validation;
 using Business.BusinessAspects.Autofac;
+using Core.Aspects.Autofac.Caching;
+using Core.Aspects.Autofac.Transaction;
+using System.Threading;
+using Core.Aspects.Autofac.Performance;
 
 namespace Business.Concrete
 {
@@ -24,14 +28,24 @@ namespace Business.Concrete
             _carDal = carDal;
 
         }
-        [SecuredOperation("car.add,admin" )]
+
+        [SecuredOperation("car.add,admin")]
         [ValidationAspect(typeof(CarValidator))]
+        [CacheRemoveAspect("ICarSerive.Get")]
+
         public IResult Add(Car car)
         {
 
             _carDal.Add(car);
 
             return new SuccessResult(Messages.Added);
+        }
+        [TransactionScopeAspect]
+        public IResult AddTransactionalTest(Car car)
+        {
+            _carDal.Update(car);
+            _carDal.Add(car);
+            return new SuccessResult(Messages.Updated);
         }
 
         public IResult Delete(Car car)
@@ -41,9 +55,11 @@ namespace Business.Concrete
         }
 
 
-
+        [CacheAspect]
+        [PerformanceAspect(5)]
         public IDataResult<List<Car>> GetAll()
         {
+            Thread.Sleep(5000);
             return new SuccessDataResult<List<Car>>(_carDal.GetAll(), Messages.Listed);
         }
 
@@ -52,13 +68,15 @@ namespace Business.Concrete
             return new SuccessDataResult<List<CarDetailDto>>(_carDal.GetAllCarDetails(), Messages.Listed);
         }
 
+        [CacheAspect]
         public IDataResult<Car> GetCarId(int id)
         {
             return new SuccessDataResult<Car>(_carDal.GetById(c => c.Id == id));
         }
 
 
-
+        [ValidationAspect(typeof(CarValidator))]
+        [CacheRemoveAspect("ICarSerive.Get")]
         public IResult Update(Car car)
         {
             _carDal.Update(car);
